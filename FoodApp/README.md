285a73169686762611d9b0ef65734109067187ae
Firoz <firozshaikh829@gmail.com>
2025-04-04 22:18:30 +0530

    testing of my App

---
+++ FoodApp/README.md
@@ -0,0 +1,28 @@
+# Redux Toolkit
+
+- Intsall @reduxjs/toolkit and react-redux
+- build our store
+- connect our store to our APP
+- Slice (cartSlice)
+- dispatch(action)
+- Selector
+
+# Types of testing
+
+- unit testing
+- integration testing
+- End to End testing - e2e testing
+
+# Setting up Testing in our App
+
+- Install React Testing Library
+- Install jest
+- install babel dependencies
+- configure Bable
+- configure parcel config file to disable bable transpilation
+- jest config - npx jest --init
+- install jsdom library
+
+- install @babel/preset-react - to make JSX work in test cases
+- include @babel/preset-react inside my bable config ..["@babel/preset-react", { runtime: "automatic" }],
+- npm install -D @testing-library/jest-dom
---
+++ FoodApp/src/Components/__tests__/Contact.test.js
@@ -0,0 +1,33 @@
+import { render, screen } from "@testing-library/react";
+import Contact from "../Contact";
+
+test("should load contact us component", () => {
+  render(<Contact />);
+  const heading = screen.getByRole("heading");
+
+  // Assertion
+  expect(heading).toBeInTheDocument();
+});
+
+test("should load button inside contact component", () => {
+  render(<Contact />);
+  const button = screen.getByRole("button");
+
+  // Assertion
+  expect(button).toBeInTheDocument();
+});
+
+test("to check placeholder name", () => {
+  render(<Contact />);
+  const inputName = screen.getByPlaceholderText("name");
+  expect(inputName).toBeInTheDocument();
+});
+
+test("to check 2 input boxes", () => {
+  render(<Contact />);
+  // Querying
+  const inputBoxes = screen.getAllByRole("textbox");
+  console.log(inputBoxes.length);
+  //Assertion
+  expect(inputBoxes.length).toBe(2);
+});
